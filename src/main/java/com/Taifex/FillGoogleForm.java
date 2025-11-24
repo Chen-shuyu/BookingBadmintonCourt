36346f11a64962e406fbcb213f6633a62b2c8fa8
shuyu <shuyu@taifex.com.tw>
2025-11-24 17:31:01 +0800

    update 表單流程

--- src/main/java/com/Taifex/FillGoogleForm.java
+++ src/main/java/com/Taifex/FillGoogleForm.java
@@ -15,15 +15,31 @@ public class FillGoogleForm {
         try {
             Thread.sleep(2000);
 
+            // 點選 紀錄電子郵件
+
+            // 點選 繼續
+
+            // 填 申請人姓名
+
+            // 填 身分證後五碼
+
+            // 填 申請人手機
+
+            // 選 申請場地
+
+            // 選 使用星期
+
+            // 選 使用時段
+
             // 文字欄位
-            WebElement textInput = driver.findElement(By.cssSelector("input[type='text']"));
-            textInput.sendKeys("這是自動填寫的內容");
+//            WebElement textInput = driver.findElement(By.cssSelector("input[type='text']"));
+//            textInput.sendKeys("這是自動填寫的內容");
 
             // 單選按鈕（選第一個）
-            WebElement radio = driver.findElement(By.cssSelector("div[role='radio']"));
-            radio.click();
+//            WebElement radio = driver.findElement(By.cssSelector("div[role='radio']"));
+//            radio.click();
 
-            Thread.sleep(1000);
+//            Thread.sleep(1000);
 
             // 點擊送出
             WebElement submit = driver.findElement(By.cssSelector("div[role='button'][aria-label='提交']"));
